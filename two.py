6f9de71a9123c1ade6dc21d3cb263022101a7053
Predsu <74704455+Predsu@users.noreply.github.com>
2023-10-22 19:27:31 +0200

    Obecna pogoda
    Skrypt do wyświetlania obecnego stanu pogody na wskazane kordynaty

---
+++ two.py
@@ -0,0 +1,61 @@
+"""Example of usage."""
+import asyncio
+import logging
+
+from aiohttp import ClientError, ClientSession
+
+from accuweather import (
+    AccuWeather,
+    ApiError,
+    InvalidApiKeyError,
+    InvalidCoordinatesError,
+    RequestsExceededError,
+)
+
+
+#cordy i api key (cordy zostają te same chyba że mamy sposób na wczytywanie obecnych)
+#ustawione na Katowice
+LATITUDE = 50.25833
+LONGITUDE = 19.0275
+API_KEY = "d8SNarC0KGzK45AAAZ9PAS1lzrLYm21U"
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+async def main():
+    """Run main function."""
+    async with ClientSession() as websession:
+        try:
+            accuweather = AccuWeather(
+                API_KEY,
+                websession,
+                latitude=LATITUDE,
+                longitude=LONGITUDE,
+                language="pl",
+            )
+            current_conditions = await accuweather.async_get_current_conditions()
+            forecast_daily = await accuweather.async_get_daily_forecast(
+                days=5, metric=True
+            )
+            forecast_hourly = await accuweather.async_get_hourly_forecast(
+                hours=12, metric=True
+            )
+        except (
+            ApiError,
+            InvalidApiKeyError,
+            InvalidCoordinatesError,
+            ClientError,
+            RequestsExceededError,
+        ) as error:
+            print(f"Error: {error}")
+        else:
+            print(f"Location: {accuweather.location_name} ({accuweather.location_key})\n") #klucz lokacji accuweather
+            print(f"Requests remaining: {accuweather.requests_remaining}\n") #nwm ale jest konieczne (chyba) to chyba możliwe do wykonania requesty na aplikację
+            print(f"Current: {current_conditions}\n") #obecny stan pogody
+            print(f"Forecast: {forecast_daily}\n") #prognoza dzienna
+            print(f"Forecast hourly: {forecast_hourly}\n") #progrnoza godzinna
+
+
+loop = asyncio.new_event_loop()
+loop.run_until_complete(main())
+loop.close()
